ec9468ea21755668d97215b0480a804dd676295d
Regine Marte <reginmarte@gmail.com>
2021-03-14 15:28:41 +1100

    merged with setup and minor fixes

--- backend/comp3900_stockoverflow/accounts/views.py
+++ backend/comp3900_stockoverflow/accounts/views.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User, auth
+from .models import User
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
 # Create your views here.
-
-
+@api_view(['POST'])
 def register(request):
+    # name = request.POST['name']
+    # email = request.POST['email']
+    #     # password1 = request.POST['password1']
+    #     # password2 = request.POST['password2']
+    #     print(name)
+    #     print(email)
+    #     user = Users.objects.create(name=name, email=email)
+    #     print(user, 'user created')
+    #     pass
+    # else:
+    #     return render(request, 'register.html')
+    # return render(request, 'register.html')
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        print(name)
-        print(email)
-        user = Users.objects.create_user(username=name, password = password1,email = email)
-        user.save();
-        print('user created')
-        pass
-    else:
-        return render(request, 'register.html');
-    return render(request, 'register.html');
+
 def add(request):
-    #val1 = request.GET['num1']
-    #val2 = request.GET['num2']
-    #res = int(val1) + int(val2)
-    return render(request,'home.html',{'result': 3})
+    # val1 = request.GET['num1']
+    # val2 = request.GET['num2']
+    # res = int(val1) + int(val2)
+    return render(request, 'home.html', {'result': 3})
