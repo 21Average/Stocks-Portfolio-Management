a051630b3d4ab184b3cd00ef2f55f9821a0aa526
ChangyongYang <dinoamous@hotmail.com>
2021-03-15 22:32:11 +1100

    add portfolio, adding and removing stocks by using codes from example

--- backend/comp3900_stockoverflow/stocks/urls.py
+++ backend/comp3900_stockoverflow/stocks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, portfolio, delete_stock
 
 urlpatterns = [
     path('', home, name='home'),
+    path('portfolio/', portfolio, name='portfolio'),
+    path('deletestock/<stock_symbol>', delete_stock, name='delete_stock'),
 ]
