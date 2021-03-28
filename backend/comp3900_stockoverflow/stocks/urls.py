4ac584f1f36b675a66e35eca71b59c9372a80187
Lucalucashhh <47741119+Lucalucashhh@users.noreply.github.com>
2021-03-28 18:51:51 +0800

    Tesla dataset and the prediction notebook

--- backend/comp3900_stockoverflow/stocks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home, portfolio, delete_stock
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('portfolio/', portfolio, name='portfolio'),
-    path('deletestock/<stock_symbol>', delete_stock, name='delete_stock'),
-]
