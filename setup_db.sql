e9684729f50ebb660d2ed42ba3212bed7456544a
21Average <liluno0909@gmail.com>
2021-04-23 16:00:57 +1000

    setup sql file

--- setup_db.sql
+++ setup_db.sql
@@ -1,3 +1,5 @@
 CREATE USER postgres WITH PASSWORD '1234';
-CREATE DATABASE 'comp3900';
+ALTER USER postgres WITH SUPERUSER;
+ALTER USER postgres CREATEDB;
+CREATE DATABASE comp3900;
 grant all privileges on database comp3900 to postgres;
